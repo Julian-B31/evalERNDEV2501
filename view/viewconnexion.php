772c00d639cc903413ac36d59e9c9472a6b678a4
Julian-B31 <julian.bourigault@gmail.com>
2025-04-02 14:01:29 +0200

    ajout de code dans viewconnexion.php

---
+++ view/viewconnexion.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./js/script.js" defer></script>
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Mon profil</title>
+</head>
+<body>
+     <h1>Se connecter<h1>
+     <form action="" method="post">
+        <label for="email">Veuillez saisir votre adresse email : </label>
+        <input type="email" name="email">
+        <label for="password">Veuillez saisir votre mot de pazsse :</label>
+        <input type="password" name="password"> 
+        <input type="submit" name="submit" value="connexion"> 
+</body>
+</html>
